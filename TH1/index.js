324682497de4d0764aec97ad863091080ec1dca5
nana <nale4095nale4095@gmail.com>
2026-04-09 17:19:13 +0700

    Sua BTH1

--- TH1/index.js
+++ TH1/index.js
@@ -1,24 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    // Thiết lập Header chung cho tất cả phản hồi
-    res.writeHead(200, {
-        'Content-Type': 'text/plain; charset=utf-8'
-    });
-
     const url = req.url;
 
+    // Sử dụng if-else và chỉ gọi writeHead bên trong từng nhánh
     if (url === '/' || url === '/home') {
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
         res.end('Trang chủ: Chào mừng bạn đến với Node.js Server của Nana!');
     } 
     else if (url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
         res.end('Trang giới thiệu: Đây là dự án thực hành Node.js đầu tiên của mình.');
     } 
     else if (url === '/contact') {
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
         res.end('Trang liên hệ: Email của mình là nale4095@gmail.com');
     } 
     else {
-        // Trường hợp không tìm thấy trang
+        // Chỉ khi không khớp các đường dẫn trên mới gửi lỗi 404
         res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
         res.end('404 - Không tìm thấy trang');
     }
